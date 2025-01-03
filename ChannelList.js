1a9169afe3859b0b22b1076933fa33a5746269ed
Cha Soyoon <socha@ncsoft.com>
2025-01-03 23:58:16 +0900

    Latest version

---
+++ ChannelList.js
@@ -0,0 +1,13 @@
+exports.ChannelList = function ChannelList() {
+	return channelList;
+}
+
+const channelList = [
+	"https://www.youtube.com/watch?v=x-J16ow5hyI&list=UUFtEEv80fQVKkD4h1PF-Xqw",
+	"https://www.youtube.com/watch?v=w2jhL5jMOCM&list=UUE_M8A5yxnLfW0KghEeajjw",
+	"https://www.youtube.com/watch?v=T43sbhCKvBY&list=UUtI0Hodo5o5dUb67FeUjDeA",
+	"https://www.youtube.com/watch?v=SKBG1sqdyIU&list=UUXZCJLdBC09xxGZ6gcdrc6A",
+	"https://www.youtube.com/watch?v=Vdro1p53LYQ&list=UUWOA1ZGywLbqmigxE4Qlvuw",
+	"https://www.youtube.com/watch?v=tjWeximdZfA&list=UUkLXELm63_pH7L-r-548kig",
+	"https://www.youtube.com/watch?v=Vcg8HjAj6dE&list=UU1sELGmy5jp5fQUugmuYlXQ",
+];
---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1710,
+    height: 1105,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+      webviewTag: true
+    }
+  })
+
+  win.setMenu(null);
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
